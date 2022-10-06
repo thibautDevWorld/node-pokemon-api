37f6425947fb985a7fa24918dd8af8c8efc8d6f5
thibautDevWorld <thibaut.lartigue.pro@gmail.com>
2022-10-06 12:48:05 +0200

    create an API REST with node/express

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const morgan = require('morgan') //middleware
+const favicon = require('serve-favicon') // middleware
+const bodyParser = require('body-parser')
+const sequelize = require('./src/db/sequelize')
+
+
+const app = express();
+const port = 3000;
+
+
+app
+    .use(favicon(__dirname + '/favicon.ico'))
+    .use(morgan('dev'))
+    .use(bodyParser.json())
+
+
+sequelize.initDb();
+
+// Ici, nous placerons nos futurs points de terminaison.
+require('./src/routes/findAllPokemons')(app)
+require('./src/routes/findPokemonByPk')(app)
+require('./src/routes/createPokemon')(app)
+
+app.listen(port, () => console.log(`Notre application Node est démarrée sur : http://localhost:${port}`));
