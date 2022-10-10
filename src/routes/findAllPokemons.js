d8d905318dbfed49dfea568b3cace06bc8a75645
thibautDevWorld <thibaut.lartigue.pro@gmail.com>
2022-10-10 11:10:08 +0200

    Add query operator and advanced request

--- src/routes/findAllPokemons.js
+++ src/routes/findAllPokemons.js
@@ -1,15 +1,40 @@
-const { Pokemon } = require('../db/sequelize')
-  
+const { Pokemon } = require("../db/sequelize");
+const { Op } = require("sequelize");
+
 module.exports = (app) => {
-  app.get('/api/pokemons', (req, res) => {
-    Pokemon.findAll()
-      .then(pokemons => {
-        const message = 'La liste des pokémons a bien été récupérée.'
-        res.json({ message, data: pokemons })
-      })
-      .catch(error => {
-        const message = `La liste des pokemons n'a pas pu être récupéreé. Réessayez dans quelques instants.`
-        res.status(500).json({ message, data: error })
-      })
-  })
-}
+  app.get("/api/pokemons", (req, res) => {
+    if (req.query.name) {
+      const name = req.query.name;
+      const limit = parseInt(req.query.limit) || 5;
+
+      if(name.length < 2) {
+        const message = 'Le terme de recherche doit contenir au moins 2 caractères.'
+        res.status(400).json({ message })
+      }
+
+      return Pokemon.findAndCountAll({
+        where: {
+          name: {
+            //'name' est la propriété du modèle pokemon
+            [Op.like]: `%${name}%`, //'name' est le critère de la recherche
+          },
+        },
+        order: ["name"],
+        limit: limit,
+      }).then(({ count, rows }) => {
+        const message = `Il y a ${count} pokemons qui correspondent au terme de recherche ${name}.`;
+        res.json({ message, data: rows });
+      });
+    } else {
+      Pokemon.findAll({ order: ["name"] })
+        .then((pokemons) => {
+          const message = "La liste des pokémons a bien été récupérée.";
+          res.json({ message, data: pokemons });
+        })
+        .catch((error) => {
+          const message = `La liste des pokemons n'a pas pu être récupéreé. Réessayez dans quelques instants.`;
+          res.status(500).json({ message, data: error });
+        });
+    }
+  });
+};
